ccd4488403d10a058924359b0785fc99034c7909
Powei Lin <poweilin1994@gmail.com>
2024-11-15 20:31:18 -0500

    clippy

--- src/board.rs
+++ src/board.rs
@@ -70,12 +70,10 @@ impl<'a> Board<'a> {
                         }
                     } else if self.found_board_idxs.contains_key(&b2)
                         && self.found_board_idxs.contains_key(&b3)
+                        && self.found_board_idxs.get(&b2).unwrap().is_some()
+                        && self.found_board_idxs.get(&b3).unwrap().is_some()
                     {
-                        if self.found_board_idxs.get(&b2).unwrap().is_some()
-                            && self.found_board_idxs.get(&b3).unwrap().is_some()
-                        {
-                            return Some((b2, b3));
-                        }
+                        return Some((b2, b3));
                     }
                 }
                 None
